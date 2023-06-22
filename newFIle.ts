37fa320e67a1d774cc248e3c89d8833963250a7d
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2023-06-22 15:19:50 +0200

    change 2

--- newFIle.ts
+++ newFIle.ts
@@ -1,7 +1,7 @@
 console.log("ts34");
 console.log("ts34");
 console.log("ts34");
-console.log("ts3aaa4");
+console.log("ts3aaaaaa4");
 console.log("ts34aaadasa");
 console.log("ts34");
 console.log("tsa34adsad");
